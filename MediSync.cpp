0bd7aa51fe2e79c978dacc0a5a7468facb48f48f
imran-2503 <imraa23@farmingdale.edu>
2025-11-19 17:06:09 -0500

    MediSync.cpp

--- MediSync.cpp
+++ MediSync.cpp
@@ -1,13 +1,22 @@
-// MediSync.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Project: MediSync
+// Purpose: Analyzing the wait time of patients
+// Author(s): Areez Imran, Sara Suhail, Daniel Barnes, Zain Sosa, Sulayman Zaraei
+//Date: 11-19-2025
+// Version: 1.0
+// 
 //
 
 #include <iostream>
+#include <ctime>
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+   //Patients
+    
 }
 
 
 
 //ROOOMS TO ADD SIMILAR TO HOW WE HAD HALLS AND LABS IN CAMPIX: Emergency Room (ER) Operating Room Patient Room ICU Room Imaging Room (covers X-ray/CT/MRI in general) Laboratory Room Recovery Room
+
